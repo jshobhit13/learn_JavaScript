e348baa14af4895d9a0a61e1c258ecebe23d2998
Shobhit Jain <154263226+jshobhit13@users.noreply.github.com>
2025-07-04 14:53:47 +0000

    Interview Prep-on dataTypes_Summary

---
+++ dataTypes_Summary.js
@@ -0,0 +1,28 @@
+//7 Types: null,String,Number,Boolean,undefined,Symbol,BigInt
+const score=100
+const scoreValue=100.3
+
+const isLoggedin=false
+const outsideTemp = null
+
+let userEmail;
+const id=Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id===anotherId);
+
+const BigInt=321456987654
+
+//Non-Primitive Types
+//Objects,Array,Functions
+ const heros=["Shaktimaan","Nagraj","Doga"]
+ let myObj={
+    name:"Shobhit",
+    age:22
+ }
+
+ const myFunction=function(){
+    console.log("Hello World");
+ }
+ console.log(typeof scoreValue);
+// JavaScript is dynamically Typed Language which means the datatype is being checked at runtime and which can be the disdvantage of it sometimes 
